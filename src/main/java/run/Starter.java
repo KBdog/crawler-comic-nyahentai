5dfe63e463f39329aac73f3be59b27c59c0b4df1
KBdog <1246450339@qq.com>
2021-05-25 16:41:17 +0800

    更新漫画名输出和输出格式

--- src/main/java/run/Starter.java
+++ src/main/java/run/Starter.java
@@ -40,7 +40,8 @@ public class Starter {
                 .replaceAll("\\]"," ")
                 .replaceAll("\\,"," ")
                 .replaceAll("\\'"," ")
-                .replaceAll("\\@"," ");
+                .replaceAll("\\@"," ")
+                .replaceAll("\\!","！");
         List<Comic> comicList=null;
         if(keyword!=null&&!keyword.equals("")){
             System.out.println("关键字:"+keyword);
